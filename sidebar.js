96edf6de92a556b1330706ca8f91deaf14ff52a4
0ushun <yoshinooushun321@gmail.com>
2025-09-05 22:27:03 +0900

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   const sidebarContainer = document.querySelector(".sidebar");
   if (sidebarContainer) {
-    fetch("/Ant_Legion/sidebar.html")
+    fetch("/Ant_Legion/sidebar.html"　+ new Date().getTime())
       .then(response => response.text())
       .then(data => {
         sidebarContainer.innerHTML = data;
